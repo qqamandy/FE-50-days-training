9113205026b26ca530a878b39500809960e4995d
Mandy Huang <mandy@osp.com.tw>
2024-03-05 15:47:55 +0800

    delete unneccessary file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
-import DashBoard from './DashBoard';
-import {Button} from '@mui/material'
+import DashBoard from "./DashBoard";
+import { Button } from "@mui/material";
+
+
 function App() {
   return (
     <div className="App">
-     <DashBoard/>
-     <Button variant='contained'>hiii</Button>
+      <DashBoard />
+      <Button variant="contained">hiii</Button>
     </div>
   );
 }
